9b279efbb099dcdb718ae15ffb7340b1724c29ba
bharat <bharat.khiani@gmail.com>
2016-09-03 21:34:03 +0530

    Used XML Dom for retrieving patient data.
    Used XmlDocument for loading xml data from file.  Fetched all the invoices
    using getElementsByTagName, then iterated each invoice and extracted
    patient name, ssn, procedures and the cost of each procedure.
    Appended all this info to a stringbuilder and later dumped stringbuilder
    contents to a multiline textbox.

---
+++ xmldoc_dom1/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace xmldoc_dom1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txbInfo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(330, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Patient Procedure and Cost information:";
+            // 
+            // txbInfo
+            // 
+            this.txbInfo.Location = new System.Drawing.Point(16, 32);
+            this.txbInfo.Multiline = true;
+            this.txbInfo.Name = "txbInfo";
+            this.txbInfo.ReadOnly = true;
+            this.txbInfo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txbInfo.Size = new System.Drawing.Size(512, 267);
+            this.txbInfo.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(540, 311);
+            this.Controls.Add(this.txbInfo);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txbInfo;
+    }
+}
+
---
+++ xmldoc_dom1/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Xml;
+
+namespace xmldoc_dom1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            string xmlFilename = Application.StartupPath + @"\invoices.xml";
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load(xmlFilename);
+
+            XmlElement root = doc.DocumentElement;
+
+            XmlNodeList allInvoices = root.GetElementsByTagName("invoice");
+            foreach (XmlNode anInvoice in allInvoices)
+            {
+                // Read patient Name.
+                XmlNode patient = anInvoice.FirstChild;
+                sb.AppendLine();//blank line.
+                sb.AppendLine("Name: " + patient.Attributes["firstname"].Value);
+                sb.AppendLine("SSN: " + patient.Attributes["SSN"].Value);
+
+                sb.AppendLine("Procedures and Cost:");
+                // Extract procedures for this invoice
+                // starting at 1 because zero was <patient>
+                for (int i = 1; i < anInvoice.ChildNodes.Count; i++)
+                {
+                    if (anInvoice.ChildNodes[i].Name.Equals("procedure"))
+                    {
+                        sb.AppendLine(anInvoice.ChildNodes[i].Attributes["name"].Value + 
+                            ", "+ anInvoice.ChildNodes[i].Attributes["cost"].Value);
+                    }
+                }
+            }// for each invoice ends.
+
+            txbInfo.Text = sb.ToString();
+
+        }
+    }
+}
